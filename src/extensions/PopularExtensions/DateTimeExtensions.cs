edd446ec6d09078b8bdb988bd682584e66a2e0bd
morteza <mz2019@gmail.com>
2018-07-11 15:02:28 +0430

    add the most PopularExtensions Project

---
+++ src/extensions/PopularExtensions/DateTimeExtensions.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PopularExtensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string GetJalaliDate(this DateTime gregorianDate)
+        {
+            System.Globalization.PersianCalendar persianCal = new System.Globalization.PersianCalendar();
+
+            string FarsiDateConverted = persianCal.GetYear(gregorianDate).ToString("0000") + "/" +
+                         persianCal.GetMonth(gregorianDate).ToString("00") + "/" +
+                         persianCal.GetDayOfMonth(gregorianDate).ToString("00");
+
+            return FarsiDateConverted;
+        }
+
+        public static string GetJalaliMonth(this DateTime gregorianDate)
+        {
+            System.Globalization.PersianCalendar persianCal = new System.Globalization.PersianCalendar();
+            int monthNo = persianCal.GetMonth(gregorianDate);
+            switch (monthNo)
+            {
+                case 1:
+                    return "فروردین";
+                case 2:
+                    return "اردیبهشت";
+                case 3:
+                    return "خرداد";
+                case 4:
+                    return "تیر";
+                case 5:
+                    return "مرداد";
+                case 6:
+                    return "شهریور";
+                case 7:
+                    return "مهر";
+                case 8:
+                    return "آبان";
+                case 9:
+                    return "آذر";
+                case 10:
+                    return "دی";
+                case 11:
+                    return "بهمن";
+                default:
+                    return "اسفند";
+
+            }
+        }
+
+        public static string GetJalaliYear(this DateTime gregorianDate)
+        {
+            System.Globalization.PersianCalendar persianCal = new System.Globalization.PersianCalendar();
+            int year = persianCal.GetYear(gregorianDate);
+            return year.ToString();
+        }
+
+        public static int GetJalaliDay(this DateTime gregorianDate)
+        {
+            System.Globalization.PersianCalendar persianCal = new System.Globalization.PersianCalendar();
+            return persianCal.GetDayOfMonth(gregorianDate);
+        }
+
+        public static string GetJalaliDayOfWeek(this DateTime gregorianDate)
+        {
+            System.Globalization.PersianCalendar persianCal = new System.Globalization.PersianCalendar();
+
+            DayOfWeek dayOfWeek = persianCal.GetDayOfWeek(gregorianDate);
+
+            switch (dayOfWeek)
+            {
+                case DayOfWeek.Saturday:
+                    return "شنبه";
+                case DayOfWeek.Sunday:
+                    return "یکشنبه";
+                case DayOfWeek.Monday:
+                    return "دوشنبه";
+                case DayOfWeek.Tuesday:
+                    return "سه شنبه";
+                case DayOfWeek.Wednesday:
+                    return "چهارشنبه";
+                case DayOfWeek.Thursday:
+                    return "پنج شنبه";
+                default:
+                    return "جمعه";
+            }
+        }
+    }
+
+}
---
+++ src/extensions/PopularExtensions/ExpressionExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace PopularExtensions
+{
+    public static class ExpressionExtensions
+    {
+        public static string GetName<T, TResult>(this Expression<Func<T, TResult>> expression)
+        {
+            return GetUnchangedName(expression).ToLower();
+        }
+
+        private static string GetUnchangedName<T, TResult>(Expression<Func<T, TResult>> expression)
+        {
+            return (expression.Body as MemberExpression ?? ((UnaryExpression)expression.Body).Operand as MemberExpression).Member.Name;
+        }
+        public static IEnumerable<string> GetListName<T, TResult>(this Expression<Func<T, TResult>> expression)
+        {
+            if (expression.Body is MemberExpression)
+            {
+                return new string[] { ((MemberExpression)expression.Body).Member.Name }.ToList();
+            }
+            else if (expression.Body is UnaryExpression)
+            {
+                return new string[] { (((UnaryExpression)expression.Body).Operand as MemberExpression).Member.Name }.ToList();
+            }
+            else
+                if (expression.Body is NewExpression)
+            {
+                var members = ((NewExpression)expression.Body).Members?.ToList();
+
+                var result = new List<string>();
+                if (members != null)
+                {
+                    foreach (var item in members)
+                    {
+                        result.Add(item.Name);
+                    }
+                }
+
+                return result;
+            }
+            throw new InvalidCastException();
+        }
+
+        public static PropertyInfo GetPropertyInfo<T, TResult>(this Expression<Func<T, TResult>> expression)
+        {
+            var name = GetUnchangedName(expression);
+
+            return typeof(T).GetProperties().FirstOrDefault(p => p.Name == name);
+        }
+    }
+
+}
